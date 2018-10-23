a39b7341a8b484c3bd8b61bbf7246bbe6ed91461
davideeparry <davideeparry@gmail.com>
2018-10-22 20:03:29 -0700

    basic callback library, still need to make the inline lambda's.

--- src/db/Database.cpp
+++ src/db/Database.cpp
@@ -3,29 +3,29 @@
 
 void Database::init(StateService &state) {
     stateService = &state;
-    if (!SD.begin(chipSelect)) {
+    if (!SD.begin(chipSelect)) { // Need a callback
         communications.sendError("SD Initialization Failed");
         return;
     }
-    systemLog = SD.open(systemLogId, FILE_WRITE); // NEED MORE ERROR CHECKING HERE
-    dataLog = SD.open(dataLogId, FILE_WRITE);
+    systemLog = SD.open(systemLogId, FILE_WRITE); // need callback
+    dataLog = SD.open(dataLogId, FILE_WRITE); // need callback
 };
 
 void Database::writeToDataLog(char* dataMsg) {
-    dataLog = SD.open(dataLogId, FILE_WRITE);
+    dataLog = SD.open(dataLogId, FILE_WRITE); // needs callback
     dataLog.println(dataMsg);
     dataLog.close();
 }
 
 void Database::writeToSystemLog(char* systemMsg) {
-    systemLog = SD.open(systemLogId, FILE_WRITE);
+    systemLog = SD.open(systemLogId, FILE_WRITE); // needs callback
     systemLog.println(systemMsg);
     systemLog.close();
 }
 
 String Database::dumpDataLogs() { // WILL WANT TO MAKE THIS ONLY DUMP AS MUCH AS MEMORY WILL ALLOW, AND IT SHOULD BE CALLED MULTIPLE TIMES BY THE CALLER
     String dataDump;
-    dataLog = SD.open(dataLogId, FILE_WRITE);
+    dataLog = SD.open(dataLogId, FILE_WRITE); // callback
     while (dataLog.available()) {
         dataDump.append(dataLog.read());
     }
